0b9783ff9a93e16a8303afb2064280148e913226
Schnitzel5 <tangphilip3@gmail.com>
2025-08-04 18:49:11 +0200

    fix

--- buildscripts/bundle_default.sh
+++ buildscripts/bundle_default.sh
@@ -30,16 +30,20 @@ sudo chmod +x gradlew
 
 unzip -o app/build/outputs/apk/release/app-release.apk -d app/build/outputs/apk/release
 
-cp app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so      ../../prefix/arm64-v8a/usr/local/lib
-cp app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so    ../../prefix/armeabi-v7a/usr/local/lib
-cp app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so            ../../prefix/x86/usr/local/lib
-cp app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so         ../../prefix/x86_64/usr/local/lib
+mkdir -p ../../prefix/arm64-v8a
+mkdir -p ../../prefix/armeabi-v7a
+mkdir -p ../../prefix/x86
+mkdir -p ../../prefix/x86_64
+cp app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so      ../../lib/arm64-v8a
+cp app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so    ../../lib/armeabi-v7a
+cp app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so            ../../lib/x86
+cp app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so         ../../lib/x86_64
 
 cd ../..
 
-zip -r default-arm64-v8a.jar                prefix/arm64-v8a/usr/local/lib/*.so
-zip -r default-armeabi-v7a.jar              prefix/armeabi-v7a/usr/local/lib/*.so
-zip -r default-x86.jar                      prefix/x86/usr/local/lib/*.so
-zip -r default-x86_64.jar                   prefix/x86_64/usr/local/lib/*.so
+zip -r default-arm64-v8a.jar                lib/arm64-v8a/*.so
+zip -r default-armeabi-v7a.jar              lib/armeabi-v7a/*.so
+zip -r default-x86.jar                      lib/x86/*.so
+zip -r default-x86_64.jar                   lib/x86_64/*.so
 
 md5sum *.jar
